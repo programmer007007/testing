baf44232cc57453f6512470016ddb98aaf7d7552
programmer007007 <35029477+programmer007007@users.noreply.github.com>
2021-08-14 09:26:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
     phpinfo();
+    // we are good from ser ver est
     // we are in the est branch local demo from pc end and from int
-    //we are a new code from the server
+    // we are a new code from the server
 ?>
