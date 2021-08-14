8963fcb77a72041207dfb01dbc1fb73ca40627a0
programmer07 <andprogrammer007@gmail.com>
2021-08-14 09:43:43 +0530

    fresh

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php 
     phpinfo();
-    // we are good from ser ver est
-    // we are in the est branch local demo from pc end and from int
-    // we are a new code from the server
-    // we are working on the sub branch
-    // we could be
-// we are good
+    // fresh
 ?>
