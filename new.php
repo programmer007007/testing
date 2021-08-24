c3c32dc9b5261e7e4488fc0b9529c62add6c9f78
programmer07 <andprogrammer007@gmail.com>
2021-08-25 00:18:36 +0530

    done work

--- new.php
+++ new.php
@@ -1,19 +1,13 @@
 <?php
 //john
 function dem(){
-<<<<<<< HEAD
 // we are the local
-=======
 // we are the server
->>>>>>> b526f75c4a166d7d5694eb8acb8e6eb2598891b9
 }
 function red(){}
 function red2(){
 
 }
 function red1(){}
-<<<<<<< HEAD
 // we are the local
-=======
 //we are the server
->>>>>>> b526f75c4a166d7d5694eb8acb8e6eb2598891b9
