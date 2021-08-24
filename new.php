29b1daecba0824406819b93b75e5cd2ce1da8444
programmer007007 <35029477+programmer007007@users.noreply.github.com>
2021-08-25 00:12:00 +0530

    Update new.php

--- new.php
+++ new.php
@@ -8,9 +8,4 @@ function red2(){
 
 }
 function red1(){}
-// we are the new changes from the server
-// we are the local changes
-// we are push
-// we are pull
-// we ar ethe new code
-// we are the lc new cord
+// we are the server
